602b7253313f2c31a8e8b070fafc67d88ac6f250
PrateekMunjal <prateekmunjal31@gmail.com>
2022-03-23 11:25:31 +0400

    added token

--- src/main.py
+++ src/main.py
@@ -16,67 +16,45 @@ limitations under the License.
 import scipy
 import numpy as np
 from typing import AnyStr, List
+from sklearn.metrics import pairwise_distances
 from slingpy import AbstractDataSource
 from slingpy.models.abstract_base_model import AbstractBaseModel
 from genedisco.active_learning_methods.acquisition_functions.base_acquisition_function import \
     BaseBatchAcquisitionFunction
 
-
-def softmax_temperature(x, temperature=1):
-    """Computes softmax probabilities from unnormalized values
-
-    Args:
-        
-        x: array-like list of energy values.
-        temperature: a positive real value.
-
-    Returns:
-        outputs: ndarray or list (dependin on x type) that is
-            exp(x / temperature) / sum(exp(x / temperature)).
-    """
-    if isinstance(x, list):
-        y = np.array(x)
-    else:
-        y = x
-    y = np.exp(y / temperature)
-    out_np = scipy.special.softmax(y)
-    if any(np.isnan(out_np)):
-        raise ValueError("Temperature is too extreme.")
-    if isinstance(x, list):
-        return [out_item for out_item in out_np]
-    else:
-        return out_np
-
-
-class SoftUncertainAcquisition(BaseBatchAcquisitionFunction):
-    def __call__(self,
-                 dataset_x: AbstractDataSource,
-                 select_size: int,
-                 available_indices: List[AnyStr], 
-                 last_selected_indices: List[AnyStr] = None, 
-                 model: AbstractBaseModel = None,
-                 temperature: float = 0.9,
-                 ) -> List:
-        avail_dataset_x = dataset_x.subset(available_indices)
-        model_pedictions = model.predict(avail_dataset_x, return_std_and_margin=True)
-
-        if len(model_pedictions) != 3:
-            raise TypeError("The provided model does not output uncertainty.")
-        
-        pred_mean, pred_uncertainties, _ = model_pedictions
-
-        if len(pred_mean) < select_size:
-            raise ValueError("The number of query samples exceeds"
-                             "the size of the available data.")
-        selection_probabilities = softmax_temperature(
-            np.log(1e-7 + pred_uncertainties ** 2), 
-            temperature
+class CoreSet(BaseBatchAcquisitionFunction):
+    def __call__(
+        self,
+        dataset_x: AbstractDataSource,
+        batch_size: int,
+        available_indices: List[AnyStr],
+        last_selected_indices: List[AnyStr],
+        last_model: AbstractBaseModel,
+    ) -> List:
+        topmost_hidden_representation = last_model.get_embedding(
+            dataset_x.subset(available_indices)
+        ).numpy()
+        selected_hidden_representations = last_model.get_embedding(
+            dataset_x.subset(last_selected_indices)
+        ).numpy()
+        chosen = self.select_most_distant(
+            topmost_hidden_representation, selected_hidden_representations, batch_size
         )
-        numerical_selected_indices = np.random.choice(
-            range(len(selection_probabilities)),
-            size=select_size,
-            replace=False,
-            p=selection_probabilities)
-        selected_indices = [available_indices[i] for i 
-                            in numerical_selected_indices]
-        return selected_indices
+        return [available_indices[idx] for idx in chosen]
+
+    def select_most_distant(self, options, previously_selected, num_samples):
+        num_options, num_selected = len(options), len(previously_selected)
+        if num_selected == 0:
+            min_dist = np.tile(float("inf"), num_options)
+        else:
+            dist_ctr = pairwise_distances(options, previously_selected)
+            min_dist = np.amin(dist_ctr, axis=1)
+
+        indices = []
+        for i in range(num_samples):
+            idx = min_dist.argmax()
+            dist_new_ctr = pairwise_distances(options, options[[idx], :])
+            for j in range(num_options):
+                min_dist[j] = min(min_dist[j], dist_new_ctr[j, 0])
+            indices.append(idx)
+        return indices
